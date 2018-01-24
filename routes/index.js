38ea120491d985abbc802c371c3379dccd447427
Ascold2017 <ascold96@gmail.com>
2018-01-24 16:00:58 +0200

    add index page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const index = require('../controllers/index');
 
+router.get('/', (req, res) => {
+    console.log('get index');
+    res.render('pages/index', {});
+})
 router.post('/sign-in', index.signIn);
 
 router.post('/registration', index.registration);
