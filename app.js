268b45665083a4bbc6ae300b8985476c1317c4ee
Ascold2017 <ascold96@gmail.com>
2018-02-19 21:14:19 +0200

    fix routes in front

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Allow crossdomain requests
 app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
